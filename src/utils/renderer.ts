6365dbd8acfa2f167393a2386ab7773224959df2
veado <61792675+veado@users.noreply.github.com>
2022-11-21 14:49:56 +0100

    Misc. UI updates (#9)
    - [x] Use `prettier-plugin-tailwindcss`
       - Note: `prettier-plugin-svelte` needs to be uninstalled to avoid incompatibilty. See https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
    - [x] Switch `tailwindcss/forms`->`tw-elements`
    - [x] VaultSortDropdown component
    - [x] Extract UI helpers into utils/renderer

---
+++ src/utils/renderer.ts
@@ -0,0 +1,48 @@
+import type { PoolStatus, VaultSort, VaultStatus, VaultType } from 'src/types/types';
+
+export const labelByPoolStatus = (status: PoolStatus) => {
+	switch (status) {
+		case 'available':
+			return 'active';
+		case 'staged':
+			return 'pending';
+		case 'suspended':
+			return 'inactive';
+		case 'unknown':
+			return 'unknown';
+	}
+};
+
+export const labelByVaultStatus = (status: VaultStatus) => {
+	switch (status) {
+		case 'ActiveVault':
+		case 'Active':
+			return 'active';
+		case 'RetiringVault':
+			return 'retiring';
+		case 'Standby':
+			return 'standby';
+		case 'unknown':
+			return 'unkown';
+	}
+};
+
+export const labelByVaultType = (type: VaultType) => {
+	switch (type) {
+		case 'asgard':
+			return 'asgard';
+		case 'ygg':
+			return 'Yggdrasil';
+		case 'bond':
+			return 'bond';
+		case 'unknown':
+			return 'unknown';
+	}
+};
+
+export const VAULT_SORT_LABEL_MAP: Record<VaultSort, string /* label*/> = {
+	usd: 'USD ↑',
+	usdRev: 'USD ↓',
+	name: 'Asset ↑',
+	nameRev: 'Asset ↓'
+};
